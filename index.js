51a45a88788c0a4eadcd7e846337cc039589dbf8
thet-naing-soe <mehnthetnaingsoe@gmail.com>
2025-02-02 23:58:07 +0630

    to work dropdown menu functionality

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // javascript
+const menuBtn = document.querySelector(".menu");
+const panel = document.querySelector(".filter");
+const panelStyles = window.getComputedStyle(panel);
+
+menuBtn.addEventListener("click", () => {
+  if (panelStyles.getPropertyValue("clip-path") === "circle(0% at 100% 0%)") {
+    panel.style.clipPath = "circle(100% at 50% 50%)";
+  } else {
+    panel.style.clipPath = "circle(0% at 100% 0%)";
+  }
+});
+document.addEventListener("mouseup", (event) => {
+  if (panelStyles.getPropertyValue("clip-path") === "circle(100% at 50% 50%)") {
+    if (!panel.contains(event.target)) {
+      panel.style.clipPath = "circle(0% at 100% 0%)";
+    }
+  }
+});
